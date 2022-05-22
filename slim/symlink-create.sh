66a275ddb68d98b2346093e177b58e111a8d80f4
rhuan-pk <rhuan.pksf@gmail.com>
2022-05-22 20:41:54 +0000

    add: slim

---
+++ slim/symlink-create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo ln -s /home/${USER}/Documents/git/cfg-bkp/slim/etc/slim.conf /etc/
+sudo ln -s /home/${USER}/Documents/git/cfg-bkp/slim/usr/share/slim/themes/my-theme /usr/share/slim/themes
