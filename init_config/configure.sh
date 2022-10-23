5d420930882d95a6c7f2d386ade4cf7da539eeaa
rhuan-pk <rhuan.pksf@gmail.com>
2022-10-23 01:02:29 -0300

    init_config/configure.sh: atualizado

--- init_config/configure.sh
+++ init_config/configure.sh
@@ -50,9 +50,7 @@ comandos_repo='comandos-linux'
 
 # Arrays
 denied_list=(\
-	'pre_install' \
 	'install_docker'\
-	'pos_install' \
 	'print_banner' \
 	'loading_message' \
 	'end_message'\
@@ -74,8 +72,8 @@ reset_color='\e[m'
 # Processo pré instalação: atualização do sistema e instalação dos pacotes via `apt`.
 pre_install() {
 	cd /tmp
-	echo ${password} | sudo -S apt update
-	echo ${password} | sudo -S apt install \
+	sudo -v apt update
+	sudo -v apt install \
 		xorg \
 		i3 \
 		rofi \
@@ -159,7 +157,7 @@ clone_repos() {
 
 # Seta o arquivo de configura de rede.
 set_network_file() {
-	echo ${password} | sudo -S tee /etc/netplan/01-netcfg.yaml <<- EOF
+	sudo -v tee /etc/netplan/01-netcfg.yaml <<- EOF
 		# This file describes the network interfaces available on your system
 		# For more information, see netplan(5).
 		network:
@@ -170,7 +168,7 @@ set_network_file() {
 
 # Seta as variáveis de ambiente do `qt`.
 set_variables_2qt() {
-	echo ${password} | sudo -S tee /etc/environment  <<- EOF
+	sudo -v tee /etc/environment  <<- EOF
 		$(cat /etc/environment)
 		export QT_QPA_PLATFORMTHEME=qt5ct
 		export QT_AUTO_SCREEN_SCALE_FACTOR=0
@@ -179,14 +177,14 @@ set_variables_2qt() {
 
 # Seta o arquivo de autostart e o script dos programas do autostart.
 set_autostart_programs() {
-	echo ${password} | sudo -S tee ${local_bin}/autostart_programs  <<- EOF
+	sudo -v tee ${local_bin}/autostart_programs  <<- EOF
 		#!/usr/bin/env bash
 
 		sleep 5; \$(which copyq) &
 		sleep 5; \$(which pcloud) &
 		sleep 5; \$(which discord) &
 	EOF
-	echo ${password} | sudo -S chmod +x ${local_bin}/autostart_programs
+	sudo -v chmod +x ${local_bin}/autostart_programs
 	cat <<- EOF > ${autostart_path}/autostart_programs.desktop
 		[Desktop Entry]
 		Type=Application
@@ -199,12 +197,12 @@ set_autostart_programs() {
 
 # Seta o novo swapfile.
 set_swapfile() {
-	echo ${password} | sudo -S swapoff /swapfile
-	echo ${password} | sudo -S rm /swapfile
-	echo ${password} | sudo -S fallocate -l 4G /swapfile
-	echo ${password} | sudo -S chmod 600 /swapfile
-	echo ${password} | sudo -S mkswap /swapfile
-	echo ${password} | sudo -S swapon /swapfile
+	sudo -v swapoff /swapfile
+	sudo -v rm /swapfile
+	sudo -v fallocate -l 4G /swapfile
+	sudo -v chmod 600 /swapfile
+	sudo -v mkswap /swapfile
+	sudo -v swapon /swapfile
 }
 
 # Seta os symlinks necessários.
@@ -216,9 +214,9 @@ set_symlinks() {
 
 # Instala o docker (depreciado).
 install_docker() {
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | echo ${password} | sudo -S gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | echo ${password} | sudo -S tee /etc/apt/sources.list.d/docker.list >/dev/null
-	echo ${password} | sudo -S apt update; echo ${password} | sudo -S apt install \
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -v gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo -v tee /etc/apt/sources.list.d/docker.list >/dev/null
+	sudo -v apt update; sudo -v apt install \
 	docker-ce \
 	docker-ce-cli \
 	containerd.io \
@@ -228,41 +226,41 @@ install_docker() {
 # Instala os programas pré compilados.
 install_portables() {
 	# toplip
-	echo ${password} | sudo -S curl -fsSLo ${local_bin}/toplip 'https://2ton.com.au/standalone_binaries/toplip'
-	echo ${password} | sudo -S chmod +x ${local_bin}/toplip
+	sudo -v curl -fsSLo ${local_bin}/toplip 'https://2ton.com.au/standalone_binaries/toplip'
+	sudo -v chmod +x ${local_bin}/toplip
 	# speedtest
 	wget -O speedtest.tgz 'https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-x86_64.tgz'
 	tar -zxvf speedtest.tgz
-	echo ${password} | sudo -S mv speedtest ${local_bin}/
+	sudo -v mv speedtest ${local_bin}/
 	# yt-dlp
-	echo ${password} | sudo -S curl -fsSLo ${local_bin}/yt-dlp 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp'
-	echo ${password} | sudo -S chmod +x ${local_bin}/yt-dlp
+	sudo -v curl -fsSLo ${local_bin}/yt-dlp 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp'
+	sudo -v chmod +x ${local_bin}/yt-dlp
 	# mdr
-	echo ${password} | sudo -S curl -fsSLo ${local_bin}/mdr 'https://github.com/MichaelMure/mdr/releases/download/v0.2.5/mdr_linux_amd64'
-	echo ${password} | sudo -S chmod +x ${local_bin}/mdr
+	sudo -v curl -fsSLo ${local_bin}/mdr 'https://github.com/MichaelMure/mdr/releases/download/v0.2.5/mdr_linux_amd64'
+	sudo -v chmod +x ${local_bin}/mdr
 }
 
 # Instala os programas `.deb`.
 install_programs() {
 	# chrome
 	wget -O google-chrome_tmp.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	echo ${password} | sudo -S dpkg -i /tmp/google-chrome_tmp.deb
-	echo ${password} | sudo -S apt install -f -y
+	sudo -v dpkg -i /tmp/google-chrome_tmp.deb
+	sudo -v apt install -f -y
 	# discord
 	wget -O discord_tmp.deb 'https://discord.com/api/download?platform=linux&format=deb'
 	sleep 5
-	echo ${password} | sudo -S dpkg -i discord_tmp.deb
-	echo ${password} | sudo -S apt install -f -y
+	sudo -v dpkg -i discord_tmp.deb
+	sudo -v apt install -f -y
 	# vs-code
-	wget -O - https://packages.microsoft.com/keys/microsoft.asc | echo ${password} | sudo -S tee /etc/apt/keyrings/packages.microsoft.asc >/dev/null
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.asc] https://packages.microsoft.com/repos/code stable main" | echo ${password} | sudo -S tee /etc/apt/sources.list.d/vscode.list >/dev/null
-	echo ${password} | sudo -S apt update; echo ${password} | sudo -S apt install code -y
-	echo ${password} | sudo -S apt install -f -y
+	wget -O - https://packages.microsoft.com/keys/microsoft.asc | sudo -v tee /etc/apt/keyrings/packages.microsoft.asc >/dev/null
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.asc] https://packages.microsoft.com/repos/code stable main" | sudo -v tee /etc/apt/sources.list.d/vscode.list >/dev/null
+	sudo -v apt update; sudo -v apt install code -y
+	sudo -v apt install -f -y
 	# sublime
-	wget -O - https://download.sublimetext.com/sublimehq-pub.gpg | echo ${password} | sudo -S tee /etc/apt/keyrings/sublimehq-pub.asc >/dev/null
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/sublimehq-pub.asc] https://download.sublimetext.com/ apt/stable/" | echo ${password} | sudo -S tee /etc/apt/sources.list.d/sublime-text.list
-	echo ${password} | sudo -S apt update; echo ${password} | sudo -S apt install sublime-text -y
-	echo ${password} | sudo -S apt install -f -y
+	wget -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo -v tee /etc/apt/keyrings/sublimehq-pub.asc >/dev/null
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/sublimehq-pub.asc] https://download.sublimetext.com/ apt/stable/" | sudo -v tee /etc/apt/sources.list.d/sublime-text.list
+	sudo -v apt update; sudo -v apt install sublime-text -y
+	sudo -v apt install -f -y
 }
 
 # Compila os programas que são disponibilizados apenas nesse formato.
@@ -270,41 +268,40 @@ compile_programs() {
 	local abiword=abiword
 	local grive2=grive2
 	# colorpicker
-	echo ${password} | sudo -S apt install libgtk2.0-dev libgdk3.0-cil-dev libx11-dev libxcomposite-dev libxfixes-dev -y
+	sudo -v apt install libgtk2.0-dev libgdk3.0-cil-dev libx11-dev libxcomposite-dev libxfixes-dev -y
 	git clone https://github.com/Jack12816/colorpicker.git
 	cd ./colorpicker
-	echo ${password} | sudo -S make -j8
-	echo ${password} | sudo -S mv ./colorpicker ${local_bin}/
+	sudo -v make -j8
+	sudo -v mv ./colorpicker ${local_bin}/
 	cd ../
 	# i3lock-color
-	echo ${password} | sudo -S apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
+	sudo -v apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
 	git clone https://github.com/Raymo111/i3lock-color.git
 	cd ./i3lock-color
 	./install-i3lock-color.sh
 	cd ../
 	# wdiff
-	echo ${password} | sudo -S apt install texinfo colordiff -y
-	mkdir ./${wdiff}/ && cd ./${wdiff}/ && curl -Lo ${wdiff}.tar.gz 'http://ftp.gnu.org/gnu/wdiff/wdiff-latest.tar.gz' && tar -zxvf ./${wdiff}.tar.gz && cd $(ls -1 | grep -E "(${wdiff}-([[:digit:]]+\.?)+)") && echo ${password} | sudo -S ./configure && echo ${password} | sudo -S make -j8 && echo ${password} | sudo -S make install
+	sudo -v apt install texinfo colordiff -y
+	mkdir ./${wdiff}/ && cd ./${wdiff}/ && curl -Lo ${wdiff}.tar.gz 'http://ftp.gnu.org/gnu/wdiff/wdiff-latest.tar.gz' && tar -zxvf ./${wdiff}.tar.gz && cd $(ls -1 | grep -E "(${wdiff}-([[:digit:]]+\.?)+)") && sudo -v ./configure && sudo -v make -j8 && sudo -v make install
 	cd ../../
 	# grive2
-	echo ${password} | sudo -S apt install git cmake build-essential libgcrypt20-dev libyajl-dev libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev debhelper zlib1g-dev dpkg-dev pkg-config -y
+	sudo -v apt install git cmake build-essential libgcrypt20-dev libyajl-dev libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev debhelper zlib1g-dev dpkg-dev pkg-config -y
 	mkdir ./${grive2}/ && cd ./${grive2}/
 	git clone 'https://github.com/vitalif/grive2' $grive2
 	cd ./${grive2}/
 	dpkg-buildpackage -j8 --no-sign
 	cd ../
-	echo ${password} | sudo -S dpkg --install ./*.deb
+	sudo -v dpkg --install ./*.deb
 	cd ../
 }
 
 # Desabilita os serviços desnecessários.
 disable_services() {
-	echo ${password} | sudo -S systemctl disable NetworkManager-wait-online.service
+	sudo -v systemctl disable NetworkManager-wait-online.service
 }
 
 pos_install() {
-	echo $password | sudo -Sv
-	pk-pleno
+	sudo -v; pk-pleno
 }
 
 # Printa a mensagem de carregamento do processo atual.
@@ -344,9 +341,9 @@ print_banner() {
 clear
 
 print_banner
-read -rsp 'Entre com a senha do usuário [echo ${password} | sudo -S]: ' password
-if ! echo ${password} | sudo -S -Sv; then
-	echo -e "${red_color}Error: senha do usuário [echo ${password} | sudo -S] incorreta!${reset_color}"
+read -rsp 'Entre com a senha do usuário [sudo]: ' password
+if ! echo ${password} | sudo -Sv; then
+	echo -e "\n${red_color}Error: senha do usuário [sudo] incorreta!${reset_color}\n"
 	exit 1
 fi
 
@@ -360,7 +357,7 @@ done
 clear
 
 print_banner
-for y_function in pre_install $(declare -F | awk '{print $3}') pos_install; do
+for y_function in pre_install $(echo $(declare -F | awk '{print $3}' | sed -E '/(pre|pos)_install/d')) pos_install; do
 	[[ ! ${denied_list[*]} =~ (${y_function}) ]] && {
 		tee -a $hit_log_file 1>> $error_log_file <<- eof
 			---------- $y_function ----------
