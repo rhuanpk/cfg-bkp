76df3e7030fe0962344c0d665ae5948feaddae29
rhuan-pk <rhuan.pksf@gmail.com>
2023-02-25 04:44:05 -0300

    atualizado: polybar/config: refresh.

--- polybar/symlink-create.sh
+++ polybar/symlink-create.sh
@@ -3,7 +3,7 @@
 home=${HOME:-"/home/${USER:-$(whoami)}"}
 polybar_local_path="${home}/.config/polybar"
 git_url='https://raw.githubusercontent.com/rhuan-pk/linux/master/scripts/.private/setload.sh'
-final_path=${PK_LOAD_CFGBKP:-$(wget -qO - $git_url | bash - 2>&- | grep -F cfg-bkp)}/polybar/config
+final_path=${PK_LOAD_CFGBKP:-$(wget -qO - $git_url | bash - 2>&- | grep -F cfg-bkp)}/polybar/config.ini
 
 symlink_create() {
 	ln -sfv $final_path ${polybar_local_path}
