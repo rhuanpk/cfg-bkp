f7e96a34659fcb3419021795a06ff377833cd256
rhuan-pk <rhuan.pksf@gmail.com>
2023-02-28 17:32:54 -0300

    atualizado: rc/.shellrc.d: "go-package-start()" agora é uma função.

--- rc/.shellrc.d
+++ rc/.shellrc.d
@@ -1,1 +1,11 @@
-alias go-start='pathway=/tmp/tmp/golang; [ -d $pathway ] && rm -rfv ${pathway:?pathway no set!}/ && { mkdir -pv $pathway; cd $pathway; echo "pwd: `pwd`"; touch ./main.go; go mod init golang; go mod tidy; }'
+go-package-start() {
+	pathway=/tmp/tmp/golang
+	[ -d $pathway ] && rm -rfv ${pathway:?pathway no set!}/ && {
+		mkdir -pv $pathway
+		cd $pathway
+		echo "pwd: `pwd`"
+		touch ./main.go
+		go mod init golang
+		go mod tidy
+	}
+}
