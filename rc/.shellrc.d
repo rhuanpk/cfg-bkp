2a6f64a9176dcfbacf38db2f011948a62aa1e24c
rhuan-pk <rhuan.pksf@gmail.com>
2023-03-01 02:05:52 -0300

    add: rc/.shellrc.d: aliases: "docker_remove_dangling_images" e renomeado o "go_package_start".

--- rc/.shellrc.d
+++ rc/.shellrc.d
@@ -1,4 +1,7 @@
-go-package-start() {
+docker_remove_dangling_images() {
+	docker image rm -f $(docker images -af 'dangling=true' | grep -F '<none>' | awk '{print $3}')
+}
+go_package_start() {
 	pathway=/tmp/tmp/golang
 	[ -d $pathway ] && rm -rfv ${pathway:?pathway no set!}/ && {
 		mkdir -pv $pathway
