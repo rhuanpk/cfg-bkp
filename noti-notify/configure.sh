16f633b7f82292044804aaf54d54a9393cd880f4
rhuan-pk <rhuan.pksf@gmail.com>
2023-08-29 13:26:56 -0300

    feat: atualizado noti defaults.

--- noti-notify/configure.sh
+++ noti-notify/configure.sh
@@ -5,4 +5,4 @@ config_path=${home}/.config/noti
 file=noti.yaml
 
 [ ! -d $config_path ] && mkdir -pv $config_path
-toplip -c 3 -d ./${file} > ${config_path}/${file}
+toplip -d ./${file} > ${config_path}/${file}
