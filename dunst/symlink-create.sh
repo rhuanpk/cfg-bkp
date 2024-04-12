a2d52e7afa837e32ebb45335a4b6dd4c21e2a82a
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-04-12 09:17:24 -0300

    bug: fix variable name in dunst symlink create script

--- dunst/symlink-create.sh
+++ dunst/symlink-create.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 home=${HOME:-"/home/${USER:-$(whoami)}"}
 dunst_local_path=${home}/.config/dunst
 git_url='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setload.sh'
 final_path=${PK_LOAD_CFGBKP:-$(wget -qO - $git_url | bash - 2>&- | grep -F cfg-bkp)}/dunst/dunstrc
 
-[ ! -d $i3_local_path ] && mkdir -p "$i3_local_path"
-ln -sfv "$final_path" "$i3_local_path"
+[ ! -d "$dunst_local_path" ] && mkdir -p "$dunst_local_path"
+ln -sfv "$final_path" "$dunst_local_path"
