faf04491e82b5d264eb9e140ac32f748f3febcf7
rhuanpk <rhuan.patriky@rhuanpk.net>
2024-06-25 15:04:22 -0300

    feat: add possible missing step

--- init-config/setup-root.sh
+++ init-config/setup-root.sh
@@ -520,6 +520,7 @@ compile-programs() {
 	apt install -y autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
 	action-repeater git clone 'https://github.com/Raymo111/i3lock-color.git'
 	cd './i3lock-color/'
+	./build.sh
 	./install-i3lock-color.sh
 	cd ../
 }
