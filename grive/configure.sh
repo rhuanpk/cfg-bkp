c8d5c40c9e04283e4eff73e57cc3708f09a12bdc
rhuanpk <dev@rhuanpk.net>
2025-07-24 14:27:36 -0300

    style: change variable name

--- grive/configure.sh
+++ grive/configure.sh
@@ -2,10 +2,10 @@
 
 script="$0"
 folder="$HOME/googleDrive"
-prefix='sudo'
+sudo='sudo'
 
-while getopts 'w' option; do
-	[ "$option" = 'w' ] && unset prefix
+while getopts 'r' option; do
+	[ "$option" = 'r' ] && unset sudo
 done
 
 shift $((OPTIND-1))
@@ -26,7 +26,7 @@ else
 	echo "$script: success in grive configuration"
 fi
 
-$prefix tee /etc/systemd/system/grive.service <<- eof
+$sudo tee /etc/systemd/system/grive.service <<- eof
 	[Unit]
 	Description=CLI synchronizer service for GoogleDrive.
 
@@ -40,7 +40,7 @@ $prefix tee /etc/systemd/system/grive.service <<- eof
 	[Install]
 	WantedBy=multi-user.target
 eof
-if ! $prefix systemctl enable grive.service; then
+if ! $sudo systemctl enable grive.service; then
 	echo "$script: error on enabling service"
 	rm -rfv "$folder"
 	exit 1
