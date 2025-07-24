bc5281e317086ec4487c2d6cbc3850498fc8f955
rhuanpk <dev@rhuanpk.net>
2025-07-24 10:55:55 -0300

    feat: update set path dunst config file

--- dunst/symlink.sh
+++ dunst/symlink.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-home=${HOME:-"/home/${USER:-$(whoami)}"}
-dunst_local_path=${home}/.config/dunst
-git_url='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setload.sh'
-final_path=${PK_LOAD_CFGBKP:-$(wget -qO - $git_url | bash - 2>&- | grep -F cfg-bkp)}/dunst/dunstrc
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+path_dunst_src="$path_cfgbkp/dunst/dunstrc"
+path_dunst_dst="$HOME/.config/dunst"
 
-[ ! -d "$dunst_local_path" ] && mkdir -p "$dunst_local_path"
-ln -sfv "$final_path" "$dunst_local_path"
+[ ! -d "$path_dunst_dst" ] && mkdir -pv "$path_dunst_dst"
+ln -sfv "$path_dunst_src" "$path_dunst_dst"
