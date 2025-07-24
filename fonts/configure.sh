e7f193c4f9b6110a21eb25795ed6e0da0d01dfa9
rhuanpk <dev@rhuanpk.net>
2025-07-24 11:23:07 -0300

    feat: upgrade fonts configure

--- fonts/configure.sh
+++ fonts/configure.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-home=${HOME:-"/home/${USER:-`whoami`}"}
-fonts_directory=${home}/Documents/fonts
+fonts_directory="$HOME/Documents/fonts"
 
 print_infos()  {
-	status=${1:?need a status to inform!}
-	[ $status = true ] && {
+	status="${1:?need a status to inform}"
+	[ "$status" = true ] && {
 		color=32
 		message=installed
 	} || {
@@ -13,46 +12,46 @@ print_infos()  {
 		message=installing
 	}
 	cat <<- eof
-		» `formatter 35 font_name`: `formatter 1 ${font_name}` (`formatter ${color} ${message}`)
-		→ `formatter 35 font_type`: ${font_type}
-		→ `formatter 35 install_path`: ${install_path}/
+		» `formatter 35 font_name`: `formatter 1 "$font_name"` (`formatter "$color" "$message"`)
+		→ `formatter 35 font_type`: $font_type
+		→ `formatter 35 install_path`: $install_path/
 
 	eof
 }
 
 formatter() {
-	formatting="${1}"
-	[[ "${formatting}" =~ ([[:digit:]]+;?)* ]] && message="${@:2}" || message="${*}"
-	echo -e "\e[${formatting}m${message}\e[m"
+	formatting="$1"
+	[[ "$formatting" =~ ([[:digit:]]+;?)* ]] && message="${@:2}" || message="$*"
+	echo -e "\e[${formatting}m$message\e[m"
 }
 
-[ ! -d $fonts_directory ] && {
-	echo 'do not finded fonts directory! exiting...'
+[ ! -d "$fonts_directory" ] && {
+	echo 'no such fonts directory'
 	exit 1
 }
 
 cd /tmp
-echo -e "\n>>> `formatter 36 fonts directory`: ${fonts_directory} !\n"
+echo -e "\n>>> `formatter 36 fonts directory`: $fonts_directory !\n"
 
-for font in ${fonts_directory}/*; do
+for font in "$fonts_directory"/*; do
 
 	font_name="`basename ${font%.?[tT][fF]}`"
-	font_type=${font#*.}
-	install_path=${home}/.local/share/fonts
+	font_type="${font#*.}"
+	install_path="$HOME/.local/share/fonts"
 
-	if [ ${font_type,,} = ttf ]; then
-		install_path+=/truetype/${font_name}
-	elif [ ${font_type,,} = otf ]; then
-		install_path+=/opentype/${font_name}
+	if [ "${font_type,,}" = 'ttf' ]; then
+		install_path+="/truetype/$font_name"
+	elif [ "${font_type,,}" = 'otf' ]; then
+		install_path+="/opentype/$font_name"
 	else
-		echo "font: `basename "${font}"`: wrong type! continuing..."
+		echo "font: `basename "$font"`: wrong type"
 		continue
 	fi
 
-	[ ! -d $install_path ] && {
+	[ ! -d "$install_path" ] && {
 		print_infos false
-		mkdir -pv ${install_path}/
-		echo -e "cp: copying file `cp -v "${font}" ${install_path}/`\n"
+		mkdir -pv "$install_path/"
+		echo -e "cp: copying file `cp -v "$font" "$install_path/"`\n"
 	} || print_infos true
 
 done
