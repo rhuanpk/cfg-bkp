4835c44075e9bbe4b9cefc11248617d103e89eb8
rhuanpk <dev@rhuanpk.net>
2025-07-24 14:29:47 -0300

    feat: upgrade swappiness setup script

--- swappiness/configure.sh
+++ swappiness/configure.sh
@@ -5,11 +5,11 @@ sudo='sudo'
 while getopts 'r' option; do
         [ "$option" = 'r' ] && unset sudo
 done
-shift $(($OPTIND-1))
+shift $((OPTIND-1))
 
-$sudo mkdir -p /etc/sysctl.d/
+$sudo mkdir -pv /etc/sysctl.d/
 
-$sudo tee -a /etc/sysctl.d/99-sysctl.conf <<- \eof
+$sudo tee -a /etc/sysctl.d/99-sysctl.conf <<- eof
 	vm.swappiness=10
 	vm.vfs_cache_pressure=75
 eof
