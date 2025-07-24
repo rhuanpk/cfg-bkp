05a606cc533583fcc73309df77d5ebcdbe7fdb3e
rhuanpk <dev@rhuanpk.net>
2025-07-24 14:35:30 -0300

    feat: update set path zsh

--- zsh/configure.sh
+++ zsh/configure.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-GIT_URL='https://raw.githubusercontent.com/rhuanpk/linux/master/scripts/.private/setload.sh'
-ln -sfv "${PK_LOAD_CFGBKP:-`wget -qO - "$GIT_URL" | bash - 2>&- | grep -F cfg-bkp`}/my-theme"/*.zsh-theme "$HOME/.oh-my-zsh/custom/themes/"
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+path_zsh="$path_cfgbkp/my-theme"
+ln -sfv "$path_zsh"/*.zsh-theme "$HOME/.oh-my-zsh/custom/themes/"
