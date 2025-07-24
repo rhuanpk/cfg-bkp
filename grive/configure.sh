16acfa8a280012902132102a7dbef97b2161c1d3
rhuanpk <dev@rhuanpk.net>
2025-07-24 11:31:15 -0300

    feat: update grive script

--- grive/configure.sh
+++ grive/configure.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
-script=${0}
-home=${HOME:-"/home/${USER:-$(whoami)}"}
-folder=${home}/googleDrive
+script="$0"
+folder="$HOME/googleDrive"
 prefix='sudo'
 
 while getopts 'w' option; do
 	[ "$option" = 'w' ] && unset prefix
 done
 
-shift $(($OPTIND-1))
+shift $((OPTIND-1))
 
-[ ! -d $folder ] && { mkdir -pv $folder; cd $folder; } || { echo 'Folder already exists?? :/'; exit 1; }
+[ ! -d "$folder" ] && {
+	mkdir -pv "$folder"
+	cd "$folder"
+} || {
+	echo "$script: folder already exists? :/"
+	exit 1
+}
+
 if ! grive -a; then
-	echo "${script}: Error in grive configuration!"
-	rm -rfv $folder
+	echo "$script: error in grive configuration"
+	rm -rfv "$folder"
 	exit 1
 else
-	echo "${script}: Success in grive configuration!"
+	echo "$script: success in grive configuration"
 fi
+
 $prefix tee /etc/systemd/system/grive.service <<- eof
 	[Unit]
-	Description=CLI synchronizer service for GoogleDrive!
+	Description=CLI synchronizer service for GoogleDrive.
 
 	[Service]
-	WorkingDirectory=${folder}
+	WorkingDirectory=$folder
 	ExecStart=grive
 	Restart=always
 	RestartSec=300
@@ -34,9 +41,9 @@ $prefix tee /etc/systemd/system/grive.service <<- eof
 	WantedBy=multi-user.target
 eof
 if ! $prefix systemctl enable grive.service; then
-	echo "${script}: Error on enabling service!"
-	rm -rfv $folder
+	echo "$script: error on enabling service"
+	rm -rfv "$folder"
 	exit 1
 else
-	echo "${script}: Success on enabling service!"
+	echo "$script: success on enabling service"
 fi
