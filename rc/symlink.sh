f531f26bca7cd7f301f66a2cd1eadfae5c365b9f
rhuanpk <dev@rhuanpk.net>
2025-07-25 09:59:29 -0300

    feat: upgrade order how rc files are loaded

--- rc/symlink.sh
+++ rc/symlink.sh
@@ -5,12 +5,25 @@ path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
 path_bash_aliases="$path_cfgbkp/rc/bash_aliases"
 path_bash_functions="$path_cfgbkp/rc/bash_functions"
 path_bash_local="$path_cfgbkp/rc/bash_local"
+path_bash_vars="$path_cfgbkp/rc/bash_vars"
 source_bash=''
 
 ln -sfv "$path_bash_aliases" "$HOME/.${path_bash_aliases##*/}"
 ln -sfv "$path_bash_functions" "$HOME/.${path_bash_functions##*/}"
-ln -sfv "$path_bash_local" "$HOME/.${path_bash_local##*/}"
 
-if ! grep -qE '^source "\$PATH_CFGBKP/rc/bashrc"$' ~/.bashrc; then
-	echo -e "\nsource \"\$PATH_CFGBKP/rc/bashrc\"" >> ~/.bashrc
+cp -fv "$path_bash_local" "$HOME/.${path_bash_local##*/}"
+cp -fv "$path_bash_vars" "$HOME/.${path_bash_vars##*/}"
+
+if ! grep -qF 'source "$HOME/.bash_vars"' ~/.profile; then
+	tee -a ~/.profile <<- eof
+
+	[ -r "$HOME/.bash_vars" ] && . "$HOME/.bash_vars"
+	eof
+fi
+
+if ! grep -qF 'source "$PATH_CFGBKP/rc/bashrc"' ~/.bashrc; then
+	tee -a ~/.bashrc <<- eof
+
+	[ -r "$PATH_CFGBKP/rc/bashrc" ] && . "$PATH_CFGBKP/rc/bashrc"
+	eof
 fi
