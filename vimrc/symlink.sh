2ed534c4cc4cc499642f5f7a087ecef153906813
rhuanpk <dev@rhuanpk.net>
2025-11-10 21:55:13 -0300

    feat: update vimcr name

--- vimrc/symlink.sh
+++ vimrc/symlink.sh
@@ -4,6 +4,6 @@ url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.priva
 [ -z "$PATH_CFGBKP" ] && source /etc/environment
 path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
 
-path_vimrc="$path_cfgbkp/vimrc/.vimrc"
+path_vimrc="$path_cfgbkp/vimrc/vimrc"
 
-ln -sfv "$path_vimrc" "$HOME/"
+ln -sfv "$path_vimrc" "$HOME/.vimrc"
