4c361c97ab068d0b1ef6506947819bc06473e760
rhuanpk <dev@rhuanpk.net>
2025-11-11 09:05:16 -0300

    feat: add wayland folder

---
+++ wayland/sway/symlink.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+[ -z "$PATH_CFGBKP" ] && source /etc/environment
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+
+path_sway_src="$path_cfgbkp/wayland/sway/config"
+path_sway_dst="$HOME/.config/sway/"
+
+mkdir -pv "$path_sway_dst"
+ln -sfv "$path_sway_src" "$path_sway_dst"
