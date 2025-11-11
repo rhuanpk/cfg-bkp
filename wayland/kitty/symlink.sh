083603b3d7b5323ce6bdb40f560b72217758ebbf
rhuanpk <dev@rhuanpk.net>
2025-11-11 13:57:31 -0300

    feat: add new dot file

---
+++ wayland/kitty/symlink.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+[ -z "$PATH_CFGBKP" ] && source /etc/environment
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+
+path_kitty="$path_cfgbkp/wayland/kitty/kitty.conf"
+
+ln -sfv "$path_kitty" "$HOME/.config/kitty/"
