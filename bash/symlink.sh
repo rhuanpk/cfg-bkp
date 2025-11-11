dd5251aefa16ed6ff5d6d49b674ab3ef55ad5e6d
rhuanpk <dev@rhuanpk.net>
2025-11-11 12:52:07 -0300

    feat: update and add new dot files

---
+++ bash/symlink.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+[ -z "$PATH_CFGBKP" ] && source /etc/environment
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+
+path_bash_aliases="$path_cfgbkp/bash/.bash_aliases"
+path_bash_functions="$path_cfgbkp/bash/.bash_functions"
+path_bash_local="$path_cfgbkp/bash/.bash_local"
+path_bash_vars="$path_cfgbkp/bash/.bash_vars"
+
+ln -sfv "$path_bash_aliases" "$HOME/${path_bash_aliases##*/}"
+ln -sfv "$path_bash_functions" "$HOME/${path_bash_functions##*/}"
+
+cp -fv "$path_bash_local" "$HOME/${path_bash_local##*/}"
+cp -fv "$path_bash_vars" "$HOME/${path_bash_vars##*/}"
+
+if ! grep -qF '. "$HOME/.bash_vars"' ~/.profile; then
+	tee -a ~/.profile <<- \eof
+
+		[ -r "$HOME/.bash_vars" ] && . "$HOME/.bash_vars"
+	eof
+fi
+
+if ! grep -qF '. "$PATH_CFGBKP/bash/.bashrc"' ~/.bashrc; then
+	tee -a ~/.bashrc <<- \eof
+
+		[ -r "$PATH_CFGBKP/bash/.bashrc" ] && . "$PATH_CFGBKP/bash/.bashrc"
+	eof
+fi
--- wayland/swaync/symlink.sh
+++ wayland/swaync/symlink.sh
@@ -4,8 +4,8 @@ url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.priva
 [ -z "$PATH_CFGBKP" ] && source /etc/environment
 path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
 
-path_sway_src="$path_cfgbkp/wayland/sway/config"
-path_sway_dst="$HOME/.config/sway"
+path_sway_src="$path_cfgbkp/wayland/swaync/style.css"
+path_sway_dst="$HOME/.config/swaync"
 
 mkdir -pv "$path_sway_dst/"
 ln -sfv "$path_sway_src" "$path_sway_dst/"
