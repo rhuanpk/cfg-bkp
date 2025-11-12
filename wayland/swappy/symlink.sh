14338b71c20b0f9ffaac51a5d662828047cad302
rhuanpk <dev@rhuanpk.net>
2025-11-12 19:34:29 -0300

    feat: add swappy package to symlinks

---
+++ wayland/swappy/symlink.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
+[ -z "$PATH_CFGBKP" ] && source /etc/environment
+path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+
+path_swappy_src="$path_cfgbkp/wayland/swappy/config"
+path_swappy_dst="$HOME/.config/swappy"
+
+mkdir -pv "$path_swappy_dst/"
+ln -sfv "$path_swappy_src" "$path_swappy_dst/"
