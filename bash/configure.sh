8df32a12205917e8045ac37e2cfeaa0c3f0bcd57
rhuanpk <dev@rhuanpk.net>
2026-02-21 16:19:47 -0300

    feat: update bash dot files

--- bash/configure.sh
+++ bash/configure.sh
@@ -3,6 +3,7 @@
 url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
 [ -z "$PATH_CFGBKP" ] && source /etc/environment
 path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
+: ${path_cfgbkp:?path cfgbkp must be set}
 
 path_bash_aliases="$path_cfgbkp/bash/.bash_aliases"
 path_bash_functions="$path_cfgbkp/bash/.bash_functions"
